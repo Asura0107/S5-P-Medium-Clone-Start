76aa002b52c2bcd81955113919a40a1a5a32140e
Asura0107 <mircoorosio@gmail.com>
2023-10-27 16:42:35 +0200

    medium-U2-W1-D5

--- event.js
+++ event.js
@@ -1,6 +1,8 @@
 const nav = document.querySelector(".nav");
+const btn = document.querySelector(".firstbtn");
 const change = function () {
   nav.style.backgroundColor = "white";
+  btn.style.backgroundColor = "#1a8917";
 };
 
 window.addEventListener("scroll", change);
