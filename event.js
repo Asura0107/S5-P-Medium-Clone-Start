24df0a58b993284a35c0c6d8bcabaf9a299b13bc
Asura0107 <mircoorosio@gmail.com>
2023-10-28 21:52:24 +0200

    medium-U2-W1-D5

--- event.js
+++ event.js
@@ -1,8 +1,16 @@
 const nav = document.querySelector(".nav");
 const btn = document.querySelector(".firstbtn");
+
 const change = function () {
-  nav.style.backgroundColor = "white";
-  btn.style.backgroundColor = "#1a8917";
+  const scrollheight = window.scrollY;
+  if (scrollheight > 400) {
+    nav.style.backgroundColor = "white";
+    btn.style.backgroundColor = "#1a8917";
+  } else {
+    // dopo che l'utente ha scrollato, se l'altezza è minore ai 400 allora il background rimane quello di default
+    nav.style.backgroundColor = "";
+    btn.style.backgroundColor = "";
+  }
 };
 
 window.addEventListener("scroll", change);
